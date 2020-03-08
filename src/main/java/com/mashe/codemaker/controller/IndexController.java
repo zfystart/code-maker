2a357af2c6cfcce2dd2c25dc1ca68c1e81a6f1b8
zfystart <2512482665@qq.com>
2020-03-08 16:47:56 +0800

    add index and bootstrap

--- src/main/java/com/mashe/codemaker/controller/IndexController.java
+++ src/main/java/com/mashe/codemaker/controller/IndexController.java
@@ -1,8 +1,16 @@
 package com.mashe.codemaker.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @author zfystart
  * @data 2020/3/7 - 22:25
  */
+@Controller
 public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
