e22e91d9b3e1914049437644be1488639d54d89c
zfystart <2512482665@qq.com>
2020-03-09 14:51:35 +0800

    第三次提交

--- src/main/java/com/mashe/codemaker/controller/AuthorizeController.java
+++ src/main/java/com/mashe/codemaker/controller/AuthorizeController.java
@@ -1,8 +1,10 @@
 package com.mashe.codemaker.controller;
 
 import com.mashe.codemaker.dto.AccessTokenDTO;
+import com.mashe.codemaker.dto.GithubUser;
 import com.mashe.codemaker.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,16 +19,27 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state ){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("Iv1.860aa3d6bbdeb725");
-        accessTokenDTO.setClient_secret("a5d284fdfd4cce4ff649150e29eaf1c683a2fa97");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
-        githubProvider.getAccessToken(accessTokenDTO);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
--- src/main/java/com/mashe/codemaker/provider/GithubProvider.java
+++ src/main/java/com/mashe/codemaker/provider/GithubProvider.java
@@ -1,12 +1,10 @@
 package com.mashe.codemaker.provider;
 
+import com.alibaba.fastjson.JSON;
 import com.mashe.codemaker.dto.AccessTokenDTO;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.mashe.codemaker.dto.GithubUser;
+import okhttp3.*;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
 
@@ -17,21 +15,37 @@ import java.io.IOException;
 @Component
 public class GithubProvider {
     public String getAccessToken(AccessTokenDTO accessTokenDTO){
-        MediaType JSON = MediaType.get("application/json; charset=utf-8");
-
-
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
 
-        RequestBody body = RequestBody.create(JSON, json);
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
         Request request = new Request.Builder()
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
                 .build();
         try (Response response = client.newCall(request).execute()) {
             String string = response.body().string();
-            System.out.println(string);
-            return string;
-        } catch (IOException e) {
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+ accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        }catch (IOException e){
+
         }
         return null;
     }
